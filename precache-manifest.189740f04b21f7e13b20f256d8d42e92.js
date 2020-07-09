5da0669776c0f9956142ff077cc1746d2d2287c9
Steve Park <ghkfkd0323@gmail.com>
2020-07-08 18:56:49 -0700

    Updates

---
+++ precache-manifest.189740f04b21f7e13b20f256d8d42e92.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "070795b98337ddb0411a1a0ac1de7cc1",
+    "url": "/react-breakingbad-api/index.html"
+  },
+  {
+    "revision": "38997f81bd19a660a2fc",
+    "url": "/react-breakingbad-api/static/css/main.5c6c21d4.chunk.css"
+  },
+  {
+    "revision": "6c8da49901015efd0f11",
+    "url": "/react-breakingbad-api/static/js/2.8301c4f3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-breakingbad-api/static/js/2.8301c4f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38997f81bd19a660a2fc",
+    "url": "/react-breakingbad-api/static/js/main.f32b89ff.chunk.js"
+  },
+  {
+    "revision": "0651ef828d9e0da1b9b1",
+    "url": "/react-breakingbad-api/static/js/runtime-main.7375604c.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/react-breakingbad-api/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/react-breakingbad-api/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/react-breakingbad-api/static/media/spinner.235b9b3e.gif"
+  }
+]);
